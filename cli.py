f267b84996c4aa0163ccf3d38c279d3af567d40b
ryleigh <ryleighspamam@gmail.con>
2023-08-13 12:15:16 -0500

    create basic gui w/o interactions

--- cli.py
+++ cli.py
@@ -5,6 +5,7 @@ import time
 now = time.strftime("%b, %d, %Y, %H:%M:%S")
 print("The time is below: ")
 print("It is", now)
+
 while True:
 
     user_action = input("Type add, show, edit, complete, or exit: ")
